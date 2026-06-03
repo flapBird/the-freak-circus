28a192dcbaf13db86c9a33aff4ae23ecd77a705d
leeswal <1846873056@qq.com>
2026-06-04 01:36:03 +0800

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,14 +29,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3R72WLDGC7" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-855DQF48TP" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-3R72WLDGC7');
+gtag('config', 'G-855DQF48TP');
 `,
           }}
         />
